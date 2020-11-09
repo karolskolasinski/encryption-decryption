42bd5de35837f7645ee51cd53378b5113a61c4e0
Karol Skolasiński <karolskolasinski@gmail.com>
2020-11-09 19:37:25 +0100

    Stage 2/6: Knowledge is key

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,17 +1,22 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String message = "we found a treasure!";
-        char[] split = message.toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        char[] split = scanner.nextLine().toCharArray();
+        int key = scanner.nextInt();
 
         for (char c : split) {
             if (c >= 97 && c <= 122) {
-                c = (char) (c + 25 - 2 * (c - 97));
+                c = c + key > 122 ?
+                        (char) (c + key - 26) :
+                        (char) (c + key);
             }
+
             System.out.print(c);
         }
-
 
     }
 }
